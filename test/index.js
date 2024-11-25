843b4f3e015c6cd62d62139824cf6e49fa915c5e
FahrezaAK <rezakhasa@gmail.com>
2024-11-25 16:32:15 +0700

    first-commit

---
+++ test/index.js
@@ -0,0 +1,37 @@
+console.log('Hello World')
+console.log('Saya reza')
+let user = 'budi' //deklarasi variabel let
+console.log(user)
+
+user = 'andi'
+console.log(user)
+
+var username = 'Rano' //deklarasi variabel let
+console.log(username)
+
+username = 'Karno'
+console.log(username)
+
+const pi = 3.14
+console.log(pi)
+
+const document = 1
+console.log(document)
+
+let nama = 'Fahreza'
+let umur = 24
+let hobby = 'olahraga'
+const jenisKelamin = 'laki-laki'
+const agama = 'islam'
+console.log("nama saya " + nama + " umur saya " + umur + 
+    " hobby saya " + hobby + " jenis kelamin " + jenisKelamin
+    + " agama saya " + agama
+)
+
+// kombinasi variabel
+console.log(`nama saya ${nama}, umur saya ${umur} hobby saya ${hobby} jenis kelamin ${jenisKelamin} agama saya ${agama}`)
+
+let a = 100
+let b = 5
+let total = a + b
+console.log(total)
